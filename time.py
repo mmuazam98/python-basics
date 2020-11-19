d70ce0464d37f30505818ef4e6c7c804504725b6
Mohammad MuaZam <mm9084@srmist.edu.in>
2020-11-19 09:45:12 +0530

    added

---
+++ time.py
@@ -0,0 +1,9 @@
+import time
+
+def getFormattedDateTime ():
+	print ("Returns a formatted and readable format for date and time")
+	return time.ctime()
+
+
+print (getFormattedDateTime())
+print (" ")
